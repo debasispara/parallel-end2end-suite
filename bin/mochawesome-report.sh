a6e0e140afde29ebd3c3e83ee094365072142894
gonazmul <gonazmul@gmail.com>
2022-05-08 15:42:05 -0400

    remote old data before generating new report

--- bin/mochawesome-report.sh
+++ bin/mochawesome-report.sh
@@ -2,6 +2,6 @@
 
 mochawesomeReportDir="results/mochawesome-report"
 ./node_modules/.bin/mochawesome-merge $mochawesomeReportDir/*.json >$mochawesomeReportDir/index.json && marge $mochawesomeReportDir/index.json --reportDir $mochawesomeReportDir --inline
-rm -rf $mochawesomeReportDir/index.json
+rm -rf $mochawesomeReportDir/*.json
 open $mochawesomeReportDir/index.html
 echo "===== D O N E ====="
